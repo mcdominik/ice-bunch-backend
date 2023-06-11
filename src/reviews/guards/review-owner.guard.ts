971979041e09370186f7bd91bcfd4bb68199b01a
mcdominik <dominik8881@wp.pl>
2023-06-11 14:27:03 +0200

    fix owner guard

--- src/reviews/guards/review-owner.guard.ts
+++ src/reviews/guards/review-owner.guard.ts
@@ -13,7 +13,7 @@ export class ReviewOwnerGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
 
-    const review = await this.reviewsService.getReviewById(request.params.id);
+    const review = await this.reviewsService.getReviewById(request.params.reviewId);
     const userId = request.user.id;
 
     if (review.userId === userId) {
